d1dff5a19a176120278d0033288e791327e2db2e
Fernanda Daza <fernanditadaza9@gmail.com>
2024-10-26 23:05:21 -0300

    bd cambios y boton buscar

--- recetas/settings.py
+++ recetas/settings.py
@@ -84,16 +84,10 @@ WSGI_APPLICATION = "recetas.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'apprecetas',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':3306,
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
